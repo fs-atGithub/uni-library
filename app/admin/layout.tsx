b39f0ed4efe4e8fe1b3c5bb2c7cf4781e1551228
fs-atGithub <filip.sostaric@protonmail.com>
2025-02-18 14:45:14 +0100

    Create new book as admin

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,3 +1,4 @@
+import { eq } from 'drizzle-orm';
 import { redirect } from 'next/navigation';
 import React, { ReactNode } from 'react';
 
@@ -5,12 +6,27 @@ import { auth } from '@/auth';
 import '@/styles/admin.css';
 import Header from '@/components/admin/Header';
 import Sidebar from '@/components/admin/Sidebar';
+import { db } from '@/database/drizzle';
+import { users } from '@/database/schema';
 
 const Layout = async ({ children }: { children: ReactNode }) => {
   // ? Active Session
   const session = await auth();
   if (!session?.user?.id) {
     redirect('/sign-in');
+  }
+
+  const isAdmin = await db
+    .select({
+      isAdmin: users.role,
+    })
+    .from(users)
+    .where(eq(users.id, session.user.id))
+    .limit(1)
+    .then((res) => res[0]?.isAdmin === 'ADMIN');
+
+  if (isAdmin) {
+    redirect('/');
   }
 
   return (
